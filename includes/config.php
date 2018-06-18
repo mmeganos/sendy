36afd18be6407f74c55420039c4888e243b9a603
samarth sikotara <samarthsikotara@gmail.com>
2018-06-18 23:55:37 +0530

    make it better

--- includes/config.php
+++ includes/config.php
@@ -4,7 +4,7 @@
 	//----------------------------------------------------------------------------------//
 	
 	/*  Set the URL to your Sendy installation (without the trailing slash) */
-	define('APP_PATH', 'http://emails.realtypublishers.com');
+	define('APP_PATH', 'http://realtypublishers.herokuapp.com');
 	
 	/*  MySQL database connection credentials (please place values between the apostrophes) */
 	$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
